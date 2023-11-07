a39ec6f9dc8d8411989b2e855bc376a4b61e240c
boombuli <150118861+boombuli@users.noreply.github.com>
2023-11-07 12:56:11 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,45 +1,25 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const urlParams = new URLSearchParams(window.location.search);
-    const textToCopy = urlParams.get("text");
+document.addEventListener("DOMContentLoaded", () => {
+    const messageElement = document.getElementById("message");
+    const copyButton = document.getElementById("copyButton");
+    const textToCopy = "Text to copy"; // Replace with the text you want to copy
 
-    if (textToCopy) {
-        copyTextToClipboard2(textToCopy);
+    copyButton.addEventListener("click", () => {
+        copyTextToClipboard(textToCopy)
+            .then(() => {
+                messageElement.style.display = "block";
+                setTimeout(() => {
+                    messageElement.style.display = "none";
+                }, 2000); // Display the message for 2 seconds
+            })
+            .catch((err) => {
+                console.error('Unable to copy text: ', err);
+            });
+    });
+
+    // Programmatically trigger a click event on the button
+    copyButton.click();
+    
+    const copyTextToClipboard = (text) => {
+        return navigator.clipboard.writeText(text);
     }
-
-    function copyTextToClipboard(text) {
-        var textArea = document.createElement("textarea");
-        textArea.value = text;
-        
-        document.body.appendChild(textArea);
-        textArea.select();
-        setTimeout(() => {
-            document.execCommand('copy');
-            console.log(1+ text);
-        }, 500);
-        console.log(2+ text);
-        //document.body.removeChild(textArea);
-
-        //displayCopyMessage();
-    }
-
-    function copyTextToClipboard2(text) {
-        navigator.clipboard.writeText(text)
-        .then(() => {
-            displayCopyMessage('Text copied to clipboard: ');
-            console.log('Text copied to clipboard: ' + text);
-        })
-        .catch((err) => {
-            console.error('Unable to copy text: ', err);
-            displayCopyMessage('Unable to copy text: ');
-        });
-    }
-
-    function displayCopyMessage(text) {
-        const messageElement = document.getElementById("message");
-        messageElement.style.display = "block";
-        setTimeout(() => {
-            messageElement.style.display = "none";
-        }, 2000); // Display the message for 2 seconds
-    }
-
 });
