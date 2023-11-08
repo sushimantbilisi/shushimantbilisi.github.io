1d5de705af82fe5636d6268e44d6297261d550c7
gepran <gepran@gmail.com>
2023-11-08 17:34:19 +0400

    small changes

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,16 +3,18 @@ document.addEventListener("DOMContentLoaded", () => {
     const textToCopy = urlParams.get("text");
 
     const messageElement = document.getElementById("copyMessage");
+    const ALERT_INNER_TEXT = document.getElementById("allert-inner-text");
     
     const TBC_BUTTON = document.getElementById("tbc-btn");
     const BOG_BUTTON = document.getElementById("bog-btn");
+    const SERVICE_FEE = document.getElementById("service-fee");
+
     
     const tbc_account = "GE27TB7924345064400003";
     const bog_account = "GE68BG0000000533443714";
 
 
-
-
+    
     const copyTextToClipboard = (text) => {
         navigator.clipboard.writeText(text)
         .then(() => {
@@ -27,10 +29,17 @@ document.addEventListener("DOMContentLoaded", () => {
     }
 
     TBC_BUTTON.addEventListener("click", () => {
+        ALERT_INNER_TEXT.innerText = "Счет скопирован!";
         copyTextToClipboard(tbc_account);
     });
 
     BOG_BUTTON.addEventListener("click", () => {
+        ALERT_INNER_TEXT.innerText = "Счет скопирован!";
+        copyTextToClipboard(bog_account);
+    });
+
+    SERVICE_FEE.addEventListener("click", () => {
+        ALERT_INNER_TEXT.innerText = "Текст скопирован!";
         copyTextToClipboard(bog_account);
     });
 
