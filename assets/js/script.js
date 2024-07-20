35a690166595b0c33b5a6dc9c2a8511582d312e2
Sushiman Tbilisi <164008501+sushimantbilisi@users.noreply.github.com>
2024-07-20 16:50:55 +0400

    Added new bank accounts

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,19 +7,40 @@ document.addEventListener("DOMContentLoaded", () => {
     
     const TBC_BUTTON = document.getElementById("tbc-btn");
     const BOG_BUTTON = document.getElementById("bog-btn");
+    const MONOBANK_BUTTON = document.getElementById("mono-btn");
+    const USDT_BUTTON = document.getElementById("usdt-btn");
     //const SERVICE_FEE = document.getElementById("service-fee");
     const RECEIVER = document.getElementById("boombuli-receiver");
 
-    
+    const tbc_scheme_url = "tbc://";
+    const bog_scheme_url = "bogmBank://";
+
     const tbc_account = "GE10TB7161436010100039";
     const bog_account = "GE38BG0000000537881034";
+    const monobank_account = "5375411506909212";
+    const usdt_account = "TBkBrw6DvrY3rNLP7RMBexr2puyjYFh6qm";
     //const service_fee = "Service Fee";
     const receiver = "Nikolaiev Dmytro";
     
-    const copyTextToClipboard = (text) => {
+    const monobank_url = "https://send.monobank.ua/5qf1cqTUnn";
+
+    const openApp = (scheme_url) => {
+        if(scheme_url){
+            setTimeout(() => {
+                if(monobank_account === "5375411506909212"){
+                    window.open(scheme_url, '_blank');
+                } else {
+                    window.location.href = scheme_url;
+                }
+            }, 500);
+        }
+    }
+
+    const copyTextToClipboard = (text, scheme_url) => {
         navigator.clipboard.writeText(text)
         .then(() => {
             messageElement.style.display = "block";
+            openApp(scheme_url);
             setTimeout(() => {
                 messageElement.style.display = "none";
             }, 3000);
@@ -31,12 +52,22 @@ document.addEventListener("DOMContentLoaded", () => {
 
     TBC_BUTTON.addEventListener("click", () => {
         ALERT_INNER_TEXT.innerText = "iBan Copied!";
-        copyTextToClipboard(tbc_account);
+        copyTextToClipboard(tbc_account, tbc_scheme_url);
     });
 
     BOG_BUTTON.addEventListener("click", () => {
         ALERT_INNER_TEXT.innerText = "iBan Copied!";
-        copyTextToClipboard(bog_account);
+        copyTextToClipboard(bog_account, bog_scheme_url);
+    });
+
+    MONOBANK_BUTTON.addEventListener("click", () => {
+        ALERT_INNER_TEXT.innerText = "iBan Copied!";
+        copyTextToClipboard(monobank_account, monobank_url);
+    });
+
+    USDT_BUTTON.addEventListener("click", () => {
+        ALERT_INNER_TEXT.innerText = "USDT Address Copied!";
+        copyTextToClipboard(usdt_account);
     });
 
     // SERVICE_FEE.addEventListener("click", () => {
